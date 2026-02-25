9791fb4062341f5fa458f8f72ddf2c06e6dd99a8
phonktown <ruslankhorolets@gmail.com>
2026-02-25 18:33:11 +0300

    feat(Portal): remove ssr redirects & props

--- pages/easy-track/index.ts
+++ pages/easy-track/index.ts
@@ -1,15 +1,3 @@
-import { GetServerSideProps } from 'next';
-import { EASY_TRACK__MOTIONS_PATH } from '../../constants/urls';
-
 export default function EasyTrackIndex() {
   return null;
 }
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  return {
-    redirect: {
-      destination: EASY_TRACK__MOTIONS_PATH,
-      permanent: false,
-    },
-  };
-};
