bb48553383358b0dd8fd7371006d72fca962ffc5
phonktown <ruslankhorolets@gmail.com>
2026-03-26 14:31:06 +0300

    feat(Portal): add test banner

---
+++ shared/components/test-mode-banner/style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const TestModeBannerWrap = styled.div`
+  background-color: var(--lido-color-warningBackground);
+  color: var(--lido-color-textDark);
+  font-size: ${({ theme }) => theme.fontSizesMap.xxs}px;
+  font-weight: 400;
+  text-align: center;
+  padding: ${({ theme }) => theme.spaceMap.md}px;
+  width: 100%;
+`;
